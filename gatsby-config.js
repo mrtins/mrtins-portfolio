f04621c60913fe5503c3046f4301583b8841aaed
Vitor Martins <vitorfigmartins@gmail.com>
2020-11-21 21:46:36 -0300

    Dependency and plugin install; Typescript config; Lint config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,85 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  siteMetadata: {
+    title: 'Vitor Martins',
+    description: 'Software Engineer',
+    siteUrl: `https://vitormartins.dev`,
+    author: 'Vitor Martins',
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-fontawesome-css`,
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        test: /\.js$|\.jsx$|\.ts$|\.tsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ['develop'],
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Vitor Martins Portfolio`,
+        short_name: `Vitor Martins`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#02aab0`,
+        display: `standalone`,
+        icon: 'static/favicon.ico',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: 'Roboto',
+              variants: ['300', '400', '500', '700'],
+            },
+            {
+              family: 'Raleway',
+              variants: ['200', '300', '400', '500', '700'],
+            },
+            {
+              family: 'Montserrat',
+              variants: ['300', '400', '500', '700'],
+            },
+            {
+              family: 'Lato',
+              variants: ['300', '400', '500', '700'],
+            },
+            {
+              family: 'Karla',
+              variants: ['300', '400', '500', '700'],
+            },
+          ],
+        },
+      },
+    },
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-offline`,
+  ],
+};
