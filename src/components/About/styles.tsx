6ac6117c1294d3da833fe4cdb0c988004443b394
Vitor Martins <vitorfigmartins@gmail.com>
2020-11-24 20:29:48 -0300

    Implementing "About" section

---
+++ src/components/About/styles.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const StyledSkillsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+export const StyledSkillImage = styled.img`
+  width: 42px;
+  height: auto;
+  margin: 0 15px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,12 +7,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/index.css';
 import '../styles/main.css';
 import Intro from '../components/Intro';
+import About from '../components/About';
 
 const Home: React.FC = () => {
   return (
     <Layout pageInfo={{ pageName: 'index' }}>
       <SEO title="Home" keywords={[`dev`, `software`, `engineer`]} lang="en" />
       <Intro />
+      <About />
     </Layout>
   );
 };
